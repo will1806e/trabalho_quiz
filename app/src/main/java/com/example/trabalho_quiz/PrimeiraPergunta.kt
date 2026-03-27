febbc3025de7817dee33b9c308e93d0030a6204b
Victor <victormatheussantosnunes12@gmail.com>
2026-03-27 12:09:45 -0300

    Rework na segunda pergunta por que tava feio demais, Otimização de codigo e ligação de score da primeira pra segunda pergunta.

--- app/src/main/java/com/example/trabalho_quiz/PrimeiraPergunta.kt
+++ app/src/main/java/com/example/trabalho_quiz/PrimeiraPergunta.kt
@@ -14,11 +14,11 @@ import com.example.trabalho_quiz.databinding.ActivityPrimeiraPerguntaBinding
 class PrimeiraPergunta : AppCompatActivity() {
 
 
-     lateinit var binding: ActivityPrimeiraPerguntaBinding
+    lateinit var binding: ActivityPrimeiraPerguntaBinding
 
-     // Pontuação pro fim do jogo
+    // Pontuação pro fim do jogo
 
-     var Score = 0
+    var Score = 0
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,76 +30,6 @@ class PrimeiraPergunta : AppCompatActivity() {
         binding = ActivityPrimeiraPerguntaBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Declarando as Variáveis dos botoes aq
-
-        val Btn1 = findViewById<ImageButton>(R.id.Btn1)
-        val Btn2 = findViewById<ImageButton>(R.id.Btn2)
-        val Btn3 = findViewById<ImageButton>(R.id.Btn3)
-        val Btn4 = findViewById<ImageButton>(R.id.Btn4)
-
-        // Função pra proxima pagina, tive que criar aqui, quando tava la em baixo não tava puxando ;-;
-        fun IrParaProximaPergunta() {
-
-            val ir = Intent(this, SegundaPergunta::class.java)
-            startActivity(ir)
-
-        }
-
-        // Respota certa, vai aumentar 1 ponto e passar para proxima pergunta
-        Btn1.setOnClickListener {
-
-
-            Score += 1
-
-            // O somzinho de Xp para acertar
-            val mp = MediaPlayer.create(this, R.raw.acertouuu)
-            mp.start()
-
-            mp.setOnCompletionListener {
-                it.release()
-            }
-            IrParaProximaPergunta()
-
-        }
-        // Resposta errada só vai puxar para proxima pergunta e tocar o som de erro
-        Btn2.setOnClickListener {
-            val mp2 = MediaPlayer.create(this, R.raw.risos)
-            mp2.start()
-
-            mp2.setOnCompletionListener {
-                it.release()
-            }
-            IrParaProximaPergunta()
-
-
-        }
-        Btn3.setOnClickListener {
-            val mp2 = MediaPlayer.create(this, R.raw.risos)
-            mp2.start()
-
-            mp2.setOnCompletionListener {
-                it.release()
-            }
-
-            IrParaProximaPergunta()
-
-
-        }
-        Btn4.setOnClickListener {
-            val mp2 = MediaPlayer.create(this, R.raw.risos)
-            mp2.start()
-
-            mp2.setOnCompletionListener {
-                it.release()
-            }
-
-            IrParaProximaPergunta()
-
-
-        }
-
-
-
 
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -107,5 +37,38 @@ class PrimeiraPergunta : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    fun RespostaCorreta(view: View) {
+        val mp = MediaPlayer.create(this, R.raw.acertouuu)
+        mp.start()
+
+        mp.setOnCompletionListener {
+            it.release()
+        }
+
+        Score += 1
+
+        val ir = Intent(this, SegundaPergunta::class.java)
+
+        ir.putExtra("SCORE", Score)
+
+        startActivity(ir)
+
+    }
+
+    fun RespostaErrada(view: View) {
+        val mp2 = MediaPlayer.create(this, R.raw.risos)
+        mp2.start()
+
+        mp2.setOnCompletionListener {
+            it.release()
+        }
+
+        val ir = Intent(this, SegundaPergunta::class.java)
+
+        ir.putExtra("SCORE", Score)
+
+        startActivity(ir)
     }
 }
--- app/src/main/java/com/example/trabalho_quiz/SegundaPergunta.kt
+++ app/src/main/java/com/example/trabalho_quiz/SegundaPergunta.kt
@@ -5,12 +5,31 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.trabalho_quiz.databinding.ActivityPrimeiraPerguntaBinding
 
 class SegundaPergunta : AppCompatActivity() {
+
+
+
+
+    // Pontuação pro fim do jogo
+
+    var Score = 0
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_segunda_pergunta)
+
+        Score = intent.getIntExtra("SCORE", 0)
+
+
+
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
